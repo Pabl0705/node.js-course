6fa6a7829e9661debd68089202e9e4feb75646e0
Pabl0705 <pabloruzteprograma@gmail.com>
2024-10-22 09:56:29 +0200

    Check OS info for scalability

---
+++ 1.os-info.js
@@ -0,0 +1,14 @@
+const os = require('node:os')
+
+console.log('Información del sistema operativo')
+console.log('--------------------------------------')
+
+console.log('Nombre del sistema operativo', os.platform())
+console.log('Versión del sistema operativo', os.release())
+console.log('Arquitectura', os.arch())
+console.log('CPUs', os.cpus())   //  <-- Mejora la escalabilidad  
+console.log('Memoria libre', os.freemem() / 1024 / 1024)
+console.log('Memoria total', os.totalmem() / 1024 / 1024)
+console.log('Tiempo encendido', os.uptime() / 60 / 60)
+
+
